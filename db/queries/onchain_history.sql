4115b50196e7517315dff570e01b060c153f6306
t3201v <truong1320vv@gmail.com>
2025-04-03 17:35:31 +0700

    fix: unique constraint onchain logs

--- db/queries/onchain_history.sql
+++ db/queries/onchain_history.sql
@@ -3,7 +3,7 @@ INSERT INTO
     onchain_histories("from","to",asset_id,token_id,amount,tx_hash,timestamp)
 VALUES (
     $1, $2, $3, $4, $5, $6, $7
-) RETURNING *;
+) ON CONFLICT DO NOTHING RETURNING *;
 
 
 -- name: GetOnchainHistoriesByTxHash :many
